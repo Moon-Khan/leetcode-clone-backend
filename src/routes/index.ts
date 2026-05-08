7a6deb31b0647a582f761a064dc4d6a923bc8aaa
Arslan Saeed <arslan.saeed@sms_holdings.com>
2026-05-08 15:43:25 +0500

    add auth base route and change commonsJs to ESModule

---
+++ src/routes/index.ts
@@ -0,0 +1,8 @@
+import type { Application, Request, Response } from "express";
+
+const initializeServices = (app: Application) => {
+   
+    app.use("/v1/auth", )
+}
+
+export default initializeServices;
