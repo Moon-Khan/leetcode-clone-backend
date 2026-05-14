50352737a18639361047984477783bcbc1cc9f15
Arslan Saeed <arslan.saeed@sms_holdings.com>
2026-05-14 20:17:43 +0500

    add base controller and a user route to get all users

--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import initializeRoutes from "./src/routes/index.js";
 
 dotenv.config();
 
@@ -9,8 +10,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({ message: "LeetCode Backend Running 🚀" });
-});
+initializeRoutes(app);
 
 export default app;
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,8 @@
 import express from "express";
-// import authController from "../controllers/authController.js";
+import authController from "../controllers/authController.js";
 
 const router = express.Router();
 
-// router.post("/login", authController.login);
-// router.post("/register", authController.register);
+router.get("/users", authController.getUsers);
 
 export default router;
